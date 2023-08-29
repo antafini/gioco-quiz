cd1ae30bc128100cfc0e85d494c5078cfec8d507
antafini <93766300+antafini@users.noreply.github.com>
2023-08-29 10:49:24 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -10,15 +10,17 @@ let currentQuestionIndex = 0;
 let currentLevel = 0;
 let score = 0;
 let timeLeft = 600;
-let timerInterval; 
+let timerInterval;
 let optionsClickable = true;
+let levels = [];
 
 // Funzione per avviare il gioco in modalità giocatore singolo
-async function startSinglePlayer() {
+function startSinglePlayer(selectedStartingLevel) {
   clearInterval(timerInterval);
+
   currentQuestionIndex = 0;
-  currentLevel = 0;
-  await loadQuestions(); // Carica le domande da JSON
+  currentLevel = selectedStartingLevel;
+  loadQuestions(); // Carica le domande da JSON senza 'await'
 
   const loadingMessage = document.getElementById("new-game-message");
   loadingMessage.style.display = "block";
@@ -29,91 +31,16 @@ async function startSinglePlayer() {
   setTimeout(() => {
     loadingMessage.style.display = "none";
     optionsClickable = true;
-    showQuestion();
+    setupGame(); // Inizializza il gioco al livello corrente
   }, 2000);
 }
 
-async function loadQuestions() {
-  try {
-    const response = await fetch("questions.json");
-    const data = await response.json();
-    levels = data;
-  } catch (error) {
-    console.error("Errore durante il caricamento delle domande:", error);
-  }
-}
-
-// Funzione per mostrare la transizione tra i livelli
-function showLevelTransitionMessage() {
-  optionsClickable = false;
-  const levelTransitionMessage = document.getElementById("level-transition-message");
-  levelTransitionMessage.style.display = "block";
-
-  setTimeout(() => {
-    levelTransitionMessage.style.display = "none";
-    optionsClickable = true;
-    showQuestion();
-  }, 2000);
-}
-
-// Funzione per inizializzare il gioco
-function setupGame() {
-  currentLevel = 0;
-  score = 0;
-  showQuestion();
-}
-
-// Funzione per mostrare la domanda corrente e le opzioni
-function showQuestion() {
-  const currentQuestion = levels[currentLevel][currentQuestionIndex];
-  questionElement.textContent = currentQuestion.text;
-  optionsElement.innerHTML = "";
-
-  currentQuestion.options.forEach((option, index) => {
-    const optionButton = document.createElement("button");
-    optionButton.textContent = option;
-    optionButton.addEventListener("click", () => {
-      if (optionsClickable) { // Verifica se le opzioni sono cliccabili
-        checkAnswer(index);
-      }
-    });
-    optionsElement.appendChild(optionButton);
-  });
-
-  const levelIndicator = document.createElement("p");
-  levelIndicator.textContent = `Livello ${currentLevel + 1}`;
-  optionsElement.appendChild(levelIndicator);
-}
-
-// Funzione per controllare la risposta data dall'utente
-function checkAnswer(selectedIndex) {
-  const currentQuestion = levels[currentLevel][currentQuestionIndex];
-  if (selectedIndex === currentQuestion.correctOption) {
-    score++;
-  }
-  currentQuestionIndex++;
-
-  if (currentQuestionIndex < levels[currentLevel].length) {
-    showQuestion();
-  } else {
-    if (currentLevel < levels.length - 1) {
-      currentLevel++;
-      currentQuestionIndex = 0;
-      showLevelTransitionMessage();
-    } else {
-      endGame();
-    }
-  }
-}
-
-// Funzione per avviare il timer
 function startTimer() {
   timeLeft = 600;
   timeLeftElement.textContent = timeLeft;
   timerInterval = setInterval(updateTimer, 1000);
 }
 
-// Funzione per aggiornare il timer e controllare se è scaduto
 function updateTimer() {
   timeLeft--;
   timeLeftElement.textContent = timeLeft;
@@ -123,7 +50,117 @@ function updateTimer() {
   }
 }
 
-// Funzione per terminare il gioco e mostrare il punteggio finale
+function changeLevel() {
+  if (currentLevel < levels.length - 1) {
+    currentLevel++;
+    currentQuestionIndex = 0;
+    showLevelTransitionMessage();
+
+    setTimeout(() => {
+      const levelTransitionMessage = document.getElementById("level-transition-message");
+      levelTransitionMessage.style.display = "none";
+      optionsClickable = true;
+      setupGame(); // Inizializza il gioco al nuovo livello
+    });
+  } else {
+    endGame();
+  }
+}
+
+async function loadQuestions() {
+  try {
+    const response = await fetch("questions.json");
+    const data = await response.json();
+    levels = data;
+    setupGame(); // Chiamata qui per inizializzare il gioco dopo aver caricato le domande
+  } catch (error) {
+    console.error("Errore durante il caricamento delle domande:", error);
+  }
+}
+
+function setupGame() {
+  score = 0;
+  showQuestion();
+}
+
+function showLevelTransitionMessage() {
+  optionsClickable = false;
+  const levelTransitionMessage = document.getElementById("level-transition-message");
+  levelTransitionMessage.style.display = "block";
+
+  document.getElementById("change-level-button").style.display = "hidden";
+  document.getElementById("end-game-button").style.display = "hidden";
+}
+
+function showQuestion() {
+  const currentLevelData = levels[currentLevel];
+
+  if (currentQuestionIndex < currentLevelData.length) {
+    const currentQuestion = currentLevelData[currentQuestionIndex];
+    questionElement.textContent = currentQuestion.text;
+    optionsElement.innerHTML = "";
+
+    currentQuestion.options.forEach((option, index) => {
+      const optionButton = document.createElement("button");
+      optionButton.textContent = option;
+      optionButton.addEventListener("click", () => {
+        if (optionsClickable) {
+          checkAnswer(index);
+        }
+      });
+      optionButton.classList.remove("correct");
+      optionButton.classList.remove("incorrect");
+
+      optionsElement.appendChild(optionButton);
+    });
+
+
+    const questionNumberIndicator = document.createElement("p");
+    questionNumberIndicator.textContent = `Domanda ${currentQuestionIndex + 1} di ${levels[currentLevel].length}`;
+    optionsElement.appendChild(questionNumberIndicator);
+
+    const levelIndicator = document.createElement("p");
+    levelIndicator.textContent = `Livello ${currentLevel + 1}`;
+    optionsElement.appendChild(levelIndicator);
+
+    if (currentLevel < levels.length - 1) {
+      document.getElementById("change-level-button").style.display = "block";
+      document.getElementById("end-game-button").style.display = "block";
+    } else {
+      document.getElementById("change-level-button").style.display = "none";
+      document.getElementById("end-game-button").style.display = "block";
+    }
+  }
+}
+
+function checkAnswer(selectedIndex) {
+  const currentLevelData = levels[currentLevel];
+
+  if (currentQuestionIndex < currentLevelData.length) {
+    const currentQuestion = currentLevelData[currentQuestionIndex];
+    const correctOptionIndex = currentQuestion.correctOption;
+
+    if (selectedIndex === correctOptionIndex) {
+      score++;
+      optionsElement.children[selectedIndex].classList.add("correct");
+    } else if (selectedIndex !== -1) {
+      optionsElement.children[selectedIndex].classList.add("incorrect");
+      optionsElement.children[correctOptionIndex].classList.add("correct");
+    }
+
+    currentQuestionIndex++;
+
+    if (currentQuestionIndex < currentLevelData.length) {
+      setTimeout(() => {
+        showQuestion();
+      }, 1000); // Mostra la domanda successiva dopo 1 secondo
+    } else {
+      showLevelTransitionMessage();
+    }
+  }
+}
+
+
 function endGame() {
   clearInterval(timerInterval);
   questionElement.textContent = "Game Over!";
@@ -131,13 +168,18 @@ function endGame() {
   scoreElement.textContent = "Score: " + score;
 
   addToLeaderboard(score);
-  
+
+  document.getElementById("change-level-button").style.display = "none";
+  document.getElementById("end-game-button").style.display = "none";
+  const levelTransitionMessage = document.getElementById("level-transition-message");
+  levelTransitionMessage.style.display = "none";
+
 }
 
-// Funzione per aggiungere il punteggio alla classifica
 function addToLeaderboard(score) {
   const listItem = document.createElement("li");
   listItem.textContent = `Score: ${score}`;
   leaderboardList.appendChild(listItem);
 }
-startSinglePlayer();
+
+startSinglePlayer(0);
